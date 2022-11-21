e85d01363822c722b2f12e0c2cde16850ab476ce
Cristian <cristiansf695@hotmail.com>
2022-11-21 10:43:50 -0300

    Product

---
+++ src/main/java/com/cristian695/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.cristian695.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cristian695.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
